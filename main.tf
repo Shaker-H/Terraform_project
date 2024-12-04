66923151630af65b8c0181aba72a759b2920bd1d
Mahmoud <mahmoudmoe53@gmail.com>
2024-12-04 13:14:35 +0000

    added the ebc app part 1 still in progress

--- main.tf
+++ main.tf
@@ -47,3 +47,30 @@ resource "aws_instance" "app_server" {
     Name = "AppServer"
   }
 }
+
+resource "aws_elastic_beanstalk_application" "example_app" {
+  name        = "<your-team-name>-task-listing-app"
+  description = "Task listing app"
+}
+
+resource "aws_elastic_beanstalk_environment" "example_app_environment" {
+  name                = "<your-team-name>-task-listing-app-environment"
+  application         = aws_elastic_beanstalk_application.example_app.name
+
+  # This page lists the supported platforms
+  # we can use for this argument:
+  # https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html#platforms-supported.docker
+  solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Docker"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.example_app_ec2_instance_profile.name
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "EC2KeyName"
+    value = "your-ec2-key-pair-name"
+  }
+}
