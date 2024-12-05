ae7ea35ceb598c08fb924bfef61dfa74ea9870b6
renate <renatepierotto@gmail.com>
2024-12-05 15:26:06 +0000

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,34 @@
 terraform {
+  backend "remote" {
+    organization = "makersproject"
 
+    workspaces {
+      name = "learn-terraform-aws"
+    }
+  }
+  
   backend "s3" {
     bucket = "group-mrs-bucket"
     key    = "group-mrs-bucket/Terraform_Project"
     region = "eu-west-2"
   }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
-
   required_version = ">= 1.2.0"
 }
-
 provider "aws" {
   region = "eu-west-2"
 }
-
 resource "aws_ecr_repository" "smrrepository" {
   name = "smrrepository"
-
   image_tag_mutability = "MUTABLE"
-
   image_scanning_configuration {
     scan_on_push = false
   }
-
   encryption_configuration {
     encryption_type = "AES256"
   }
@@ -36,13 +36,9 @@ resource "aws_ecr_repository" "smrrepository" {
     ignore_changes = [name] // I added this line so that if ecr repo already exists it will not make another one with same name
   }
 }
-
-
-
 resource "aws_instance" "app_server" {
   ami           = "ami-0d26eb3972b7f8c96"
   instance_type = "t2.micro"
-
   tags = {
     Name = "MRS_AppServer"
   }
@@ -53,13 +49,12 @@ resource "aws_elastic_beanstalk_application" "example_app" {
 }
 resource "aws_iam_role" "example_app_ec2_role" {
   name = "example-app-ec2-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
         Principal = {
           Service = "ec2.amazonaws.com"
         }
@@ -67,48 +62,42 @@ resource "aws_iam_role" "example_app_ec2_role" {
     ]
   })
 }
-
 resource "aws_iam_instance_profile" "example_app_ec2_instance_profile" {
   name = "example-app-ec2-instance-profile"
   role = aws_iam_role.example_app_ec2_role.name
 }
-
 resource "aws_elastic_beanstalk_environment" "example_app_environment" {
-  name        = "MRS-task-listing-app-environment"
-  application = aws_elastic_beanstalk_application.example_app.name
-
+  name                = "MRS-task-listing-app-environment"
+  application         = aws_elastic_beanstalk_application.example_app.name
   # This page lists the supported platforms
   # we can use for this argument:
   # https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html#platforms-supported.docker
   solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Docker"
-
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
     value     = aws_iam_instance_profile.example_app_ec2_instance_profile.name
   }
-
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
-    name      = "EC2KeyName"
-    value     = "MRS"
+    name = "EC2KeyName"
+    value = "MRS"
   }
 }
-
+# Corrected the IAM role attachment to reference the IAM role created
 resource "aws_iam_role_policy_attachment" "beanstalk_ec2_ecr_policy" {
-  role       = "aws-elasticbeanstalk-ec2-role"
+  role       = aws_iam_role.example_app_ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
-
 resource "aws_db_instance" "rds_app" {
-  allocated_storage   = 10
-  engine              = "postgres"
-  engine_version      = "17.2"
-  instance_class      = "db.t3.micro"
-  identifier          = "mrs-app-prod"
-  db_name             = "mrs_db"
-  username            = "root"
-  password            = "password"
-  skip_final_snapshot = true
-  publicly_accessible = true
+  allocated_storage    = 10
+  engine               = "postgres"
+  engine_version       = "17.2"
+  instance_class       = "db.t3.micro"
+  identifier           = "mrs-app-prod"
+  db_name              = "mrs_db"
+  username             = "root"
+  password             = "password"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
 }
